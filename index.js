760fe08640c3e67a9250e218db533c06c3d93e87
danielben <bendaniel20@gmail.com>
2020-10-26 16:44:43 +0100

    fixed lint errors.

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var http = require("http")
+var http = require('http')
 
 var server = http.createServer(function (request, response) {
-  response.writeHead(200, { "Content-Type": "text/plain" })
-  response.end("Hello World! << Capstone Project >>.")
+  response.writeHead(200, { 'Content-Type': 'text/plain' })
+  response.end('Hello World! << Capstone Project >>.')
 })
 
 var port = 80
 server.listen(port)
 
-console.log("Server running at http://localhost:%d", port)
+console.log('Server running at http://localhost:%d', port)
