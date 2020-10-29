8a8890cafa2a620d8570a8f205a6d01139e9e694
danielben <bendaniel20@gmail.com>
2020-10-29 15:12:49 +0100

    Updated the readme. removed images from the repository.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http')
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end('Hello World! << Capstone Project >> This is the green deployment.')
+  response.end('Hello World! << Capstone Project >> This is the green deployment 2.')
 })
 
 var port = 80
